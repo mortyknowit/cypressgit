ab3e1ceb906832c05a7d0ae1af9768f0ab0bec9e
mortyknowit <mortyknowit@gmail.com>
2022-12-01 14:08:56 +0100

    adding config

--- cypressgit/cypress/integration/todomvc.spec.js
+++ cypressgit/cypress/integration/todomvc.spec.js
@@ -1,5 +1,5 @@
 /// <reference types="cypress" />
 
-it ('should navigate to the TodoMVC app', () => {
-    cy.visit('http://todomvc-app-for-testing-surge.sh')
-    }) 
+it("should navigate to the TodoMVC app", () => {
+  cy.visit("http://localhost:8080");
+});
